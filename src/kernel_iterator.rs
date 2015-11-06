d84f6918b6d9febf145a01ec8d1e7358c4fb5959
Alexander Samoilov <alexander.samoilov@gmail.com>
2015-11-06 17:57:27 +0000

    `valid_state()` and `advance_state()`.

--- src/kernel_iterator.rs
+++ src/kernel_iterator.rs
@@ -48,12 +48,33 @@ impl<'b> KernelIterator<'b> {
         KernelIterator { num_kernels: num_kernels, state : (0, 0), ref_mod: ref_mod,
                          full_rank: full_rank, reduced_rank: reduced_rank}
     }
+
+    fn valid_state(&self) -> bool {
+        let (model_index, kernel_index) = self.state;
+        model_index < self.ref_mod.num_models && kernel_index < self.full_rank[model_index]
+    }
+
+    fn advance_state(&mut self) -> bool {
+        let (model_index, kernel_index) = self.state;
+        if kernel_index+1 < self.reduced_rank[model_index] {
+            self.state = (model_index, kernel_index + 1);
+            true
+        } else if model_index < self.ref_mod.num_models {
+            self.state = (model_index + 1, kernel_index);
+            true
+        } else {
+            false
+        }
+    }
 }
 
 impl<'c> Iterator for KernelIterator<'c> {
     type Item = Vec<Complex<f64>>;
 
     fn next(&mut self) -> Option<Vec<Complex<f64>>> {
+        let (model_index, kernel_index) = self.state;
+        let m = self.ref_mod;
+        //if 
         None
     }
 }
@@ -74,5 +95,15 @@ fn main() {
 
     let mut ki = KernelIterator::new(17, &mod_array);
 
-    println!("KernelIterator is constructed and properly initialized:m {:?}", ki);
+    println!("KernelIterator is constructed and properly initialized: {:?}", ki);
+
+    let mut done = false;
+    while !done {
+        let kernel = ki.next();
+        match kernel {
+            Some(_) => done = false,
+            None    => done = true
+        }
+    }
 }
+
