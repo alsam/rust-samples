5bdd7af7399525f4434e19d2168cf7b15473fc7a
Alexander Samoilov <alexander.samoilov@gmail.com>
2015-11-12 01:34:28 +0300

    implementation for `scl` finished. FIX array bounds, coonstants to UC.

--- src/scalar_conservation_law.rs
+++ src/scalar_conservation_law.rs
@@ -1,12 +1,13 @@
 //***********************************************************************
 //  Copyright 2006 John A. Trangenstein
 //
-//  This software is made available for research and instructional use 
-//  only. 
-//  You may copy and use this software without charge for these 
-//  non-commercial purposes, provided that the copyright notice and 
-//  associated text is reproduced on all copies.  
-//  For all other uses (including distribution of modified versions), 
+//  This software is made available for research and instructional use
+//  only.e available for research and instructional use
+//  only.
+//  You may copy and use this software without charge for these
+//  non-commercial purposes, provided that the copyright notice and
+//  associated text is reproduced on all copies.
+//  For all other uses (including distribution of modified versions),
 //  please contact the author at
 //    John A. Trangenstein
 //    Department of Mathematics
@@ -15,9 +16,9 @@
 //    USA
 //  or
 //    johnt@math.duke.edu
-//  
+//
 //  This software is made available "as is" without any assurance that it
-//  is completely correct, or that it will work for your purposes.  
+//  is completely correct, or that it will work for your purposes.
 //  Use the software at your own risk.
 //***********************************************************************
 
@@ -25,19 +26,19 @@
 // was graciously granted by Prof. John A. Trangenstein
 
 // The MIT License (MIT)
-// 
+//
 // Copyright (c) 2015 Alexander Samoilov
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in all
 // copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -64,8 +65,40 @@ struct Options {
 
 fn do_computation(nsteps: usize, ncells: usize, tmax: f64, ifirst: usize, ilast: usize,
                   statelft: f64, statergt: f64, velocity: f64, dt: f64,
-                  fc: isize, lc: usize, flux: &mut Vec<f64>, x: &Vec<f64>, u: &mut Vec<f64>)
+                  fc: usize, lc: usize, x: &Vec<f64>, u: &mut Vec<f64>)
 {
+    let mut istep   =   0;
+    let mut t       =   0.0f64;
+    let mut flux    =   vec![0.0f64; x.len()];
+
+    // loop over timesteps
+    while istep < nsteps && t < tmax {
+
+        // right boundary condition: outgoing wave
+        for ic in ncells .. lc {
+            u[ic]=u[ncells-1];
+        }
+        // left boundary condition: specified value
+        for ic in 0 .. fc {
+            u[ic]=statelft;
+        }
+
+        // upwind fluxes times dt (ie, flux time integral over cell side)
+        // assumes velocity > 0
+        let vdt=velocity*dt;
+        for ie in ifirst .. ilast+1 {
+            flux[ie]=vdt*u[ie-1];
+        }
+
+        // conservative difference
+        for ic in ifirst .. ilast {
+            u[ic] -= (flux[ic+1]-flux[ic]) / (x[ic+1]-x[ic])
+        }
+
+        // update time and step number
+        t       +=  dt;
+        istep   +=  1
+    }
 }
 
 fn main() {
@@ -118,7 +151,7 @@ fn main() {
     // array bounds:
     let   nsteps           = options.num_steps;
     let   ncells           = options.num_cells;
-    const fc       : isize = -2;
+    const fc       : usize = 2;
     let   lc               = ncells+1;
     const ifirst   : usize = 0;
     let   ilast            = ncells-1;
@@ -131,7 +164,6 @@ fn main() {
 
     let mut u    = vec![0.0f64; ncells+4];
     let mut x    = vec![0.0f64; ncells+1];
-    let mut flux = vec![0.0f64; ncells+1];
 
     //  uniform mesh:
     let dx = (x_right-x_left) / ncells as f64;
@@ -147,20 +179,6 @@ fn main() {
         println!("ijump : {}", ijump);
     }
 
-    // left state to left of jump
-    for ic in ifirst .. ijump-1 {
-        u[ic+3] = statelft;
-    }
-
-    // volume-weighted average in cell containing jump
-    let frac = (jump-x_left-ijump as f64 *dx)/(x_right-x_left);
-    u[ijump+3] = statelft*frac+statergt*(1.0f64-frac);
-
-    // right state to right of jump
-    for ic in ijump+1 .. ilast {
-        u[ic+3]=statergt;
-    }
-
     // stable timestep (independent of time for linear advection):
     let mut mindx = 1.0e300f64;
     for ic in ifirst .. ilast {
@@ -168,17 +186,28 @@ fn main() {
     }
     let dt = cfl*mindx/velocity.abs();
 
-    let istep = 0;
-    let t = 0.0f64;
-
     let d = Duration::span(||{
         for r in 0..options.num_runs {
             if options.verbose {
                 println!("run number : {}", r);
             }
 
+            // left state to left of jump
+            for ic in ifirst .. ijump-1 {
+                u[ic+3] = statelft;
+            }
+
+            // volume-weighted average in cell containing jump
+            let frac = (jump-x_left-ijump as f64 *dx)/(x_right-x_left);
+            u[ijump+3] = statelft*frac+statergt*(1.0f64-frac);
+
+            // right state to right of jump
+            for ic in ijump+1 .. ilast {
+                u[ic+3]=statergt;
+            }
+
             do_computation(nsteps, ncells, tmax, ifirst, ilast,
-                           statelft, statergt, velocity, dt, fc, lc, &mut flux, &x, &mut u);
+                           statelft, statergt, velocity, dt, fc, lc, &x, &mut u);
         }
     });
     println!("elapsed time: {}", d.as_secs() as f64 + d.subsec_nanos() as f64 / 1.0e9f64);
