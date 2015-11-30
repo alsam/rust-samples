c4181962bf3e84f9de6e64250b2b7238e9c81bc1
Alexander Samoilov <alexander.samoilov@gmail.com>
2015-11-30 09:36:23 +0000

    added data_path.

--- src/fft_kernels.rs
+++ src/fft_kernels.rs
@@ -8,6 +8,7 @@ use argparse::{ArgumentParser, StoreTrue, Store};
 
 struct Arguments {
     verbose: bool,
+    data_path: String,
     tol: f64,
     iter: usize,
     readonly: bool,
@@ -16,25 +17,38 @@ struct Arguments {
 impl Arguments {
     // default values for arguments
     fn new() -> Arguments {
-        Arguments { verbose: false, tol: 1e-5, iter: 1, readonly: false }
+        Arguments { verbose: false, data_path: String::from(""), tol: 1e-5, iter: 1, readonly: false }
     }
 }
 
+struct Header {
+    dummy: usize
+}
+
+fn read_header(namein: &str, data_path: &String) -> Header {
+    let name = format!("{}/{}.bin",data_path,namein);
+    let mut file = File::open(name).unwrap();
+
+    Header {dummy: 0}
+}
+
 fn main() {
-    let mut arguments = Arguments::new();
+    let mut args = Arguments::new();
     {
         let mut ap = ArgumentParser::new();
         ap.set_description("SOCS standalone test");
-        ap.refer(&mut arguments.verbose)
+        ap.refer(&mut args.verbose)
             .add_option(&["-v", "--verbose"], StoreTrue,
             "be verbose");
-        ap.refer(&mut arguments.tol)
+        ap.refer(&mut args.data_path)
+          .add_option(&["-d", "--data-path"], Store, "path to input data");
+        ap.refer(&mut args.tol)
             .add_option(&["-t", "--tol"], Store,
             "set tolerance <eps fp value>");
-        ap.refer(&mut arguments.iter)
+        ap.refer(&mut args.iter)
             .add_option(&["-i", "--iter"], Store,
             "set # of iterations");
-        ap.refer(&mut arguments.readonly)
+        ap.refer(&mut args.readonly)
             .add_option(&["-r", "--readonly"], StoreTrue,
             "set readonly");
         match ap.parse_args() {
@@ -45,9 +59,9 @@ fn main() {
         }
     }
 
-    if arguments.verbose {
-        println!("number of iterations : {} tolerance : {:e} readonly : {}",
-                 arguments.iter, arguments.tol, arguments.readonly);
+    if args.verbose {
+        println!("data path : {} number of iterations : {} tolerance : {:e} readonly : {}",
+                 args.data_path, args.iter, args.tol, args.readonly);
     }
 
     let mut file = File::open("Cargo.toml").unwrap();
