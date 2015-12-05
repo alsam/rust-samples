4dcdc3bdc30b5be18e8223a62963f00693e65ac8
Alexander Samoilov <alexander.samoilov@gmail.com>
2015-12-05 11:50:42 +0000

    refactor `new` via `GridTrait`.

--- src/generics.rs
+++ src/generics.rs
@@ -11,12 +11,23 @@ use std::iter::FromIterator;
 type c32 = num::Complex<f32>;
 type c64 = num::Complex<f64>;
 
+trait GridTrait<T> {
+    // a constructor
+    fn new() -> Grid<T> { Grid {points: Vec::new() } }
+
+    // unformatted read from the buffer
+    fn read(&mut self,
+            little_endian: bool,
+            xsize: usize,
+            ysize: usize,
+            grid_buf: &Vec<u8>);
+}
+
 struct Grid<T> {
     points: Vec<Vec<T>>,
 }
 
-impl Grid<f32> {
-    fn new() -> Self { Grid {points: Vec::new() } }
+impl GridTrait<f32> for Grid<f32> {
     fn read(&mut self,
             little_endian: bool,
             xsize: usize,
@@ -36,8 +47,7 @@ impl Grid<f32> {
     }
 }
 
-impl Grid<c32> {
-    fn new() -> Self { Grid {points: Vec::new() } }
+impl GridTrait<c32> for Grid<c32> {
     fn read(&mut self,
             little_endian: bool,
             xsize: usize,
@@ -60,8 +70,7 @@ impl Grid<c32> {
     }
 }
 
-impl Grid<f64> {
-    fn new() -> Self { Grid {points: Vec::new() } }
+impl GridTrait<f64> for Grid<f64> {
     fn read(&mut self,
             little_endian: bool,
             xsize: usize,
@@ -81,8 +90,7 @@ impl Grid<f64> {
     }
 }
 
-impl Grid<c64> {
-    fn new() -> Self { Grid {points: Vec::new() } }
+impl GridTrait<c64> for Grid<c64> {
     fn read(&mut self,
             little_endian: bool,
             xsize: usize,
@@ -107,5 +115,9 @@ impl Grid<c64> {
 
 fn main() {
     let mut grid = Grid::<f32>::new();
-    grid.read(true, 8, 8, &vec![0u8; 64 * 4]);
+    grid.read(true, 8, 8, &vec![0u8; 64 * mem::size_of::<f32>()]);
+
+    let mut grid1 = Grid::<c32>::new();
+    grid1.read(true, 8, 8, &vec![0u8; 64 * mem::size_of::<c32>()]);
 }
+
