365e75cc96589db0b115404c6d63229a01fd0601
Alexander Samoilov <alexander.samoilov@gmail.com>
2015-12-09 17:09:18 +0000

    switch on Backend::AF_BACKEND_CPU for af_sample.rs.

--- src/af_sample.rs
+++ src/af_sample.rs
@@ -8,6 +8,7 @@ fn main() {
     let num_rows: u64 = 5;
     let num_cols: u64 = 3;
     let dims = Dim4::new(&[num_rows, num_cols, 1, 1]);
+    set_backend(Backend::AF_BACKEND_CPU);
     println!("Create a 5-by-3 matrix of random floats on the GPU");
     let a = match randu(dims, Aftype::F32) {
         Ok(value) => value,
