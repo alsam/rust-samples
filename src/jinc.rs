f9b58a9ecd2a506b944b8d00fb6346c3707f1239
Alexander Samoilov <alexander.samoilov@gmail.com>
2016-01-20 01:16:06 +0300

    test `jinc` for `0.61`.

--- src/jinc.rs
+++ src/jinc.rs
@@ -1,10 +1,21 @@
 extern crate num;
 
 extern crate special_fun;
+extern crate roots;
 
 use special_fun::FloatSpecial;
+use std::f64::consts::PI;
+use roots::find_root_secant;
+
+fn jinc(r: f64) -> f64 {
+    let pi2r = 2.0f64*PI*r;
+    //2.0f64*1.0f64.besselj(pi2r) / pi2r
+    2.0f64*pi2r.besselj(1.0f64) / pi2r
+}
 
 fn main() {
     let v = 0.5f64.besselj(1.0);
     println!("besselj(1.0, 0.5) : {}", v);
+    println!("jinc(0.61) : {}", jinc(0.609835));
+
 }
