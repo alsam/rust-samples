440808d5f99a2fa9d21e43faf642931fbb3a7b87
Alexander Samoilov <alexander.samoilov@gmail.com>
2016-03-11 18:13:01 +0000

    added sample for tiny (broken now).

---
+++ src/tiny.rs
@@ -0,0 +1,17 @@
+extern crate num;
+use num::{Num, Zero, Float};
+
+//
+// trait IsTiny {
+//     fn is_tiny<T: Num>(v: T) -> bool;
+// }
+// 
+// impl IsTiny for f32 {
+//     fn is_tiny<T: Num+Float>(v: T) -> bool {
+//         ! (v.abs() > v.min_value())
+//     }
+// }
+
+fn main() {
+
+}
