6a39d06499f7c26ae45f968bdd90b00cd28d7759
Alexander Samoilov <alexander.samoilov@gmail.com>
2016-03-15 16:58:10 +0000

    write asy plot for integral in jincs sample.

--- src/jinc.rs
+++ src/jinc.rs
@@ -6,8 +6,12 @@ extern crate special_fun;
 extern crate roots;
 extern crate integration;
 
-use special_fun::FloatSpecial;
+use std::io;
+use std::fs::{File, OpenOptions};
+use std::io::{Read, Write};
 use std::f64::consts::PI;
+use std::iter::FromIterator;
+use special_fun::FloatSpecial;
 use roots::SimpleConvergency;
 use roots::find_root_brent;
 use integration::quadrature;
@@ -41,6 +45,44 @@ fn f(x: f64) -> f64 {
     quadrature::gauss_legendre(&|z: f64| z.besselj(1.0), 0.0, x, 1e-12)
 }
 
+ 
+fn write_plot_asy(fname: &str, x_label: &str, y_label: &str, x: &[f64], y: &[f64]) -> Result<(), io::Error> {
+    let mut f = try!(File::create(fname));
+    try!(f.write(br#"
+import graph;
+
+size(600,600,IgnoreAspect);
+
+real[] x = {"#));
+
+    for x_elem in x {
+        try!(write!(f, "{}, ", x_elem));
+    }
+
+    try!(f.write(br#"
+};
+real[] y = {"#));
+
+    for y_elem in y {
+        try!(write!(f, "{}, ", y_elem));
+    }
+
+    try!(f.write(br#"
+};
+
+draw(graph(x, y),  green, "inlined shared memory", MarkFill[0]);
+xaxis("r", BottomTop());
+yaxis(rotate(90)*"$\int_{z=0}^{z=\textbf{r}}J_1(z)dz$", LeftRight(), RightTicks(Label(fontsize(6pt)),
+      new real[]{0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2}) );
+
+xequals(1, red+Dotted);
+
+"#));
+
+    Ok(())
+
+}
+
 fn main() {
     let v = 0.5f64.besselj(1.0);
     println!("besselj(1.0, 0.5) : {}", v);
@@ -50,4 +92,11 @@ fn main() {
     println!("jinc roots : {:?}", jinc_roots);
     println!("f({}) = {}", 1000., f(1000.));
     println!("f({}) = {}", 3000., f(3000.));
+
+    const NPOINTS : usize = 350;
+    const SUBBANDS : usize = 20;
+    let x = Vec::from_iter ( (0..NPOINTS*SUBBANDS) .map(|i| (i as f64) / (SUBBANDS as f64)) );
+    let y = Vec::from_iter ( (0..NPOINTS*SUBBANDS) .map(|i| f(x[i]) ) );
+
+    write_plot_asy("J1_int.asy", "", "", &x, &y);
 }
