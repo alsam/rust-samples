fd8c97370a3aa8c144a0490daca2a42c414dc256
Alexander Samoilov <alexander.samoilov@gmail.com>
2020-02-21 19:19:35 +0300

    expand on gf_arm_android.

--- gf_arm_android/src/main.rs
+++ gf_arm_android/src/main.rs
@@ -1,7 +1,69 @@
 #[macro_use]
 extern crate clap;
 
+use std::fs::File;
+use std::io::{Read,Write};
+use std::str::from_utf8;
+
 // [Macro clap::clap_app](https://docs.rs/clap/2.33.0/clap/macro.clap_app.html)
+
+fn gpu_freq_stat(tail: &str) -> String {
+    format!("/sys/class/devfreq/gpufreq/{}", tail)
+}
+
+fn ddr_freq_stat(tail: &str) -> String {
+    format!("/sys/class/devfreq/ddrfreq/{}", tail)
+}
+
+fn cpu_freq_stat(id: usize, tail: &str) -> String {
+    format!("/sys/devices/system/cpu/cpu{}/cpufreq/{}", id, tail)
+}
+
+struct AvailableFrequencies
+{
+    gpu: Vec<u64>,
+    ddr: Vec<u64>,
+    cpu_little: Vec<u64>,
+    cpu_medium: Vec<u64>,
+    cpu_big: Vec<u64>,
+}
+
+struct ActualFrequencies
+{
+    gpu: u64,
+    ddr: u64,
+    cpu_little: u64,
+    cpu_medium: u64,
+    cpu_big: u64,
+}
+
+impl ActualFrequencies {
+    fn cons() -> ActualFrequencies {
+        ActualFrequencies { gpu: 0, ddr: 0, cpu_little: 0, cpu_medium: 0, cpu_big: 0, }
+    }
+
+    fn init(gpu: u64, ddr: u64, cpu_l: u64, cpu_m: u64, cpu_b: u64) -> ActualFrequencies {
+        ActualFrequencies { gpu: gpu, ddr: ddr, cpu_little: cpu_l, cpu_medium: cpu_m, cpu_big: cpu_b, }
+    }
+}
+
+fn read_sys_record(sys_path: &str) -> String {
+    let mut read_buf = String::new();
+    if let Ok(mut file) = File::open(sys_path) {
+        let sz = file.read_to_string(&mut read_buf).expect("cannot read sysfs");
+        assert!(sz > 0);
+    }
+    read_buf.trim_right().to_string()
+}
+
+fn read_sys_as_u64(sys_path: &str) -> u64 {
+    let str_buf = read_sys_record(sys_path);
+    str_buf.parse().expect("cannot parse to u64")
+}
+
+fn read_gpu_frequency() -> u64 {
+    read_sys_as_u64(&gpu_freq_stat("cur_freq"))
+}
 
 fn main() {
     let matches = clap_app!(gf =>
@@ -9,13 +71,18 @@ fn main() {
         (author: "Alexander Samoilov <alexander.samoilov@gmail.com>")
         (about: "ARM Android cross-compile sample project")
         (@arg AVAILABLE: -a --available "display available frequencies")
+        (@arg SHOW: -s --show "show current frequencies")
         (@arg SET_GPU: -g --gpu +takes_value "set GPU frequency")
-        (@arg LOCK_FREQ: -l --lock ... +takes_value "lock GPU/DDR/CPU frequencies as GPU/DDR/CPUlittle/CPUmiddle/CPUbig")
+        (@arg LOCK_FREQ: -l --lock ... +takes_value "lock GPU/DDR/CPU frequencies")
         (@arg VERBOSE: -v --verbose "be verbose")
     ).get_matches();
 
     if matches.is_present("VERBOSE") {
         println!("gf_freq command line args matches: {:?}", matches);
+    }
+
+    if matches.is_present("SHOW") {
+        println!("show device current frequencies: gpu: {}", read_gpu_frequency());
     }
 
     if let Ok(gpu_freq) = value_t!(matches, "SET_GPU", u64) {
