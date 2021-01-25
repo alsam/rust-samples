2e9abcc45b73aee8226f010fe430facf8a3fd46e
Alexander Samoilov <alexander.samoilov@gmail.com>
2021-01-25 17:20:31 +0000

    const array indexing is stable.

--- src/const_array_indexing.rs
+++ src/const_array_indexing.rs
@@ -1,12 +1,24 @@
-#![feature(const_indexing)]
-
-// used to have
-// const_arr.rs:5:24: 5:30 error: array length constant evaluation error: unsupported constant expr [E0250]
-// const_arr.rs:5    const ARR2: [usize; ARR[1]] = [42, 99];
-//                                       ^~~~~~
-// const_arr.rs:5:24: 5:30 help: run `rustc --explain E0250` to see a detailed explanation
-
 fn main() {
     const ARR: [usize; 5] = [1, 2, 3, 4, 5];
     const ARR2: [usize; ARR[1]] = [42, 99];
 }
+
+// Compiling playground v0.0.1 (/playground)
+// warning: constant is never used: `ARR`
+//  --> src/main.rs:2:5
+//   |
+// 2 |     const ARR: [usize; 5] = [1, 2, 3, 4, 5];
+//   |     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+//   |
+//   = note: `#[warn(dead_code)]` on by default
+// 
+// warning: constant is never used: `ARR2`
+//  --> src/main.rs:3:5
+//   |
+// 3 |     const ARR2: [usize; ARR[1]] = [42, 99];
+//   |     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+// 
+// warning: 2 warnings emitted
+// 
+//     Finished dev [unoptimized + debuginfo] target(s) in 0.73s
+//      Running `target/debug/playground`
