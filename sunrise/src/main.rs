d37ba882742a1e14cb3f44c458213510c591b8b1
Alexander Samoilov <alexander.samoilov@gmail.com>
2021-12-12 17:03:56 +0300

    finished sunrise sample

--- sunrise/src/main.rs
+++ sunrise/src/main.rs
@@ -4,19 +4,35 @@ extern crate clap;
 use clap::{Arg, App, SubCommand};
 
 extern crate chrono;
-use chrono::prelude::DateTime;
-use chrono::{Utc, Local};
+use chrono::prelude::{DateTime};
+use chrono::{Utc, Local, NaiveDate, Datelike};
 use std::time::{SystemTime, UNIX_EPOCH, Duration};
 use std::convert::TryInto;
+
+fn timestamp_to_time(timestamp: i64) -> String {
+    // https://stackoverflow.com/questions/50072055/converting-unix-timestamp-to-readable-time-string-in-rust
+
+    // Creates a new SystemTime from the specified number of whole seconds
+    let d = UNIX_EPOCH + Duration::from_secs(timestamp.try_into().unwrap());
+    // Create DateTime from SystemTime
+    //let datetime = DateTime::<Utc>::from(d);
+    let datetime = DateTime::<Local>::from(d);
+    // Formats the combined date and time with the specified format string.
+    //let timestamp_str = datetime.format("%Y-%m-%d %H:%M:%S.%f").to_string();
+    let timestamp_str = datetime.format("%H:%M:%S").to_string();
+    timestamp_str
+}
 
 fn main() {
     let matches = App::new("sunrise-exe")
                           .version("1.0")
                           .about("prints out sunrise and sunset time")
                           .args_from_usage(
-                              "-c, --config=[FILE] 'Sets a custom config file'
-                              <INPUT>              'Sets the input file to use'
-                              -v...                'Sets the level of verbosity'")
+                              "-c, --config=[FILE]        'Sets a custom config file'
+                              -l, --longitude=[LONGITUDE] 'Sets the place longitude'
+                              -t, --latitude=[LATITUDE]   'Sets the place latitude'
+                              -d, --date=[DATE]           'Sets the date in local timezone'
+                              -v...                       'Sets the level of verbosity'")
                           .subcommand(SubCommand::with_name("test")
                                       .about("controls testing features")
                                       .version("1.3")
@@ -24,22 +40,34 @@ fn main() {
                                       .arg_from_usage("-d, --debug 'Print debug information'"))
                           .get_matches();
 
+    // Vary the output based on how many times the user used the "verbose" flag
+    // (i.e. 'myprog -v -v -v' or 'myprog -vvv' vs 'myprog -v'
+    let verbosity_level = matches.occurrences_of("v");
 
     // Gets a value for config if supplied by user, or defaults to "default.conf"
     let config = matches.value_of("config").unwrap_or("default.conf");
-    println!("Value for config: {}", config);
+    if verbosity_level >= 2 {
+        println!("Value for config: {}", config);
+    }
 
-    // Calling .unwrap() is safe here because "INPUT" is required (if "INPUT" wasn't
-    // required we could have used an 'if let' to conditionally get the value)
-    println!("Using input file: {}", matches.value_of("INPUT").unwrap());
+    let longitude_str = matches.value_of("longitude").unwrap_or("55.4544");
+    let latitude_str = matches.value_of("latitude").unwrap_or("37.3763");
 
-    // Vary the output based on how many times the user used the "verbose" flag
-    // (i.e. 'myprog -v -v -v' or 'myprog -vvv' vs 'myprog -v'
-    match matches.occurrences_of("v") {
-        0 => println!("No verbose info"),
-        1 => println!("Some verbose info"),
-        2 => println!("Tons of verbose info"),
-        3 | _ => println!("Don't be crazy"),
+    let longitude = longitude_str.parse::<f64>().unwrap();
+    let latitude = latitude_str.parse::<f64>().unwrap();
+    if verbosity_level >= 2 {
+        println!("longitude {} latitude {}", longitude, latitude);
+    }
+
+    // https://stackoverflow.com/questions/61179070/rust-chrono-parse-date-string-parseerrornotenough-and-parseerrortooshort
+    let date_str = matches.value_of("date").unwrap_or("1970-01-01");
+    if verbosity_level >= 3 {
+        println!("Value for date: {}", date_str);
+    }
+    let naive_date = NaiveDate::parse_from_str(date_str, "%Y-%m-%d").unwrap();
+    if verbosity_level >= 1 {
+        println!("Parsed date: {:?} {} {} {}",
+            &naive_date, naive_date.year(), naive_date.month(), naive_date.day());
     }
 
     // You can handle information about subcommands by requesting their matches by name
@@ -54,23 +82,20 @@ fn main() {
 
     // Calculate times for December 11, 2021 in Moscow
     let (sunrise, sunset) = sunrise::sunrise_sunset(
-        55.4544,
-        37.3763,
-        2021,
-        12,
-        11,
+        longitude,          // 55.4544,
+        latitude,           // 37.3763,
+        naive_date.year(),  // 2021,
+        naive_date.month(), // 12,
+        naive_date.day(),   // 11,
     );
-    println!("sunrise: {} sunset: {}", sunrise, sunset);
+    if verbosity_level >= 3 {
+        println!("sunrise: {} sunset: {}", sunrise, sunset);
+    }
 
-    // https://stackoverflow.com/questions/50072055/converting-unix-timestamp-to-readable-time-string-in-rust
+    let sunrise_str = timestamp_to_time(sunrise);
+    let sunset_str = timestamp_to_time(sunset);
 
-    // Creates a new SystemTime from the specified number of whole seconds
-    let d = UNIX_EPOCH + Duration::from_secs(sunrise.try_into().unwrap());
-    // Create DateTime from SystemTime
-    //let datetime = DateTime::<Utc>::from(d);
-    let datetime = DateTime::<Local>::from(d);
-    // Formats the combined date and time with the specified format string.
-    let timestamp_str = datetime.format("%Y-%m-%d %H:%M:%S.%f").to_string();
-    println!{"{}",timestamp_str};
+    println!{"sunrize {} and sunset {} at {:?} for ({} {})",
+        sunrise_str, sunset_str, &naive_date, longitude, latitude};
 
 }
